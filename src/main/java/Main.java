d3b3bf763a97d7b83376490a2f91a8e8c504d4e0
maksim.pavliy1@gmail.com <maksim.pavliy1@gmail.com>
2021-06-26 14:54:31 +0300

    Initial commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,14 +8,13 @@ import java.util.stream.Stream;
 
 public class Main {
 
-    private static boolean IS_DONE = false;
     private static final BlockingQueue<String> targetFilePaths = new ArrayBlockingQueue<>(1, true);
     public static String mask;
     public static String path;
     public static int depth;
+    public static String targetFile;
 
-
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         System.out.print("Enter mask: ");
         mask = in.nextLine();
@@ -23,38 +22,49 @@ public class Main {
         path = in.nextLine();
         System.out.print("Enter depth: ");
         depth = in.nextInt();
+        in.close();
+
         new SearchingThread().start();
         new ShowingThread().start();
+
     }
 
     public static class SearchingThread extends Thread {
         @Override
         public void run() {
-            try {
-                Stream<Path> filesTraverse = Files.walk(Path.of(path));
+                try {
+                    Stream<Path> filesTraverse = Files.walk(Path.of(path));
 
-                filesTraverse.filter(p -> depth == p.getNameCount() - Path.of(path).getNameCount())
-                        .filter(p -> p.getFileName().toString().contains(mask))
-                        .forEach(p -> {
-                            try {
-                                targetFilePaths.put(p.toString());
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                        });
-                filesTraverse.onClose(() -> IS_DONE = true);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+                    filesTraverse.filter(p -> depth == p.getNameCount() - Path.of(path).getNameCount())
+                            .filter(p -> p.getFileName().toString().contains(mask))
+                            .forEach(p -> {
+                                try {
+                                    targetFilePaths.put(p.toString());
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                            });
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    targetFilePaths.put("done");
+                    stop();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
         }
     }
 
     public static class ShowingThread extends Thread {
         @Override
         public void run() {
-            while (!IS_DONE) {
+            while (true) {
                 try {
-                    System.out.println(targetFilePaths.take());
+                    targetFile = targetFilePaths.take();
+                    if (targetFile == "done")  stop();
+                    System.out.println(targetFile);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
